81db9140e756df1d6e5cba4a55db78d101a0bdef
SFM Milionários <79432958+zxjoaq1067@users.noreply.github.com>
2021-07-16 13:03:40 -0300

    Update simpleCommandBot.py

--- examples/simpleCommandBot.py
+++ examples/simpleCommandBot.py
@@ -6,20 +6,20 @@ logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s
 logger = logging.getLogger(__name__)
 
 def start(bot, update):
-    bot.send_message(chat_id=update.message.chat_id, text='Hello ' + update.message.from_user['first_name'] + '!' '\nI am a *Bot*, click on /help to find out how I can assist you.' , parse_mode = 'Markdown')
+    bot.send_message(chat_id=update.message.chat_id, text='Olá, ' + update.message.from_user['first_name'] + '!' '\nEu sou o *Bot*, Rei dos sonegadores de impostos! click on /help to find out how I can assist you.' , parse_mode = 'Markdown')
 
 def help(bot, update):
-    update.message.reply_text("Currently I can't help you with anything, talk to my developers and tell them to add more functions and CommandHandlers!")
+    update.message.reply_text("Atualmente não posso te ajudar em nada, fale com meus desenvolvedores e diga a eles para adicionar mais funções e CommandHandlers!")
 
 def error(bot, update, error):
     logger.warning('Update "%s" caused error "%s"', update, error)
     
 def unknown(bot, update):
-     bot.send_message(chat_id=update.message.chat_id, text="Sorry, I didn't understand that command, please click at /help to see a list of all available commands.")
+     bot.send_message(chat_id=update.message.chat_id, text="Não entendi esse comando, clique em / help para ver uma lista de todos os comandos disponíveis.")
 
 
 """Start the bot."""  
-updater = Updater(token='123456:AAABBCCDDEEEFFFGGHHHIIIJJKKLL') # Copy the Token from the Botfather here
+updater = Updater(token='1830770762:AAHZzyG7BiwhnZJK_q07pkZBD1KEeQKI804') # Copy the Token from the Botfather here
 dp = updater.dispatcher
 
 dp.add_handler(CommandHandler('start', start)) # Runs the start function declared above when a user writes /start
